a267d21a68e1f561cd60920a13f8e21505fdd015
gana <ganesha.swc@gmail.com>
2025-09-12 21:23:27 +0530

    Removed duplicate folder Day2-aws-simulations

--- Day2-aws-simulations/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "ganesh_ec2" {
-  ami                 = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "Ganesh-Terraform-EC2"
-  }
-}
--- Day2-aws-simulations/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region       = "ap-south-1"
-aws_profile       = "ganesh-dev"
-instance_type   = "t2.micro"
-ami_id              = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (Mumbai)
