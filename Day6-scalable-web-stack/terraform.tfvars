d178a45427b3fb15f7b33a4924288a50b3b49d3c
gana <ganesha.swc@gmail.com>
2025-09-16 22:17:47 +0530

    Day 6: Modular VPC and ALB setup with public/private subnets, IGW, and HTTP listener for scalable web architecture

---
+++ Day6-scalable-web-stack/terraform.tfvars
@@ -0,0 +1,9 @@
+aws_region            = "ap-south-1"
+aws_profile           = "ganesh-prod"
+vpc_cidr              = "10.0.0.0/16"
+vpc_name              = "day6-vpc"
+environment           = "Prod"
+public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"]
+azs                   = ["ap-south-1a", "ap-south-1b"]
+
---
+++ Day6-scalable-web-stack/variables.tf
@@ -0,0 +1,39 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Name tag for the VPC"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (Dev, Staging, Prod)"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDRs for public subnets"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDRs for private subnets"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability zones for subnets"
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile name"
+  type        = string
+}
