59233e5b5fb69e3e186ffe9f46dcf6ed74530922
gana <ganesha.swc@gmail.com>
2025-12-01 21:41:48 +0530

    Update values

--- Day7-mini-project/variables.tf
+++ Day7-mini-project/variables.tf
@@ -28,6 +28,12 @@ variable "ec2_name" {
   type        = string
 }
 
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for EC2 instance"
+}
+
+
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
