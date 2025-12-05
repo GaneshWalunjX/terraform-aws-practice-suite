46d9e90520d26332f556f99e74547b5ab897292b
gana <ganesha.swc@gmail.com>
2025-12-05 21:29:29 +0530

    Add variables for dynamodb

---
+++ Day7-mini-project/modules/dynamodb/variables.tf
@@ -0,0 +1,15 @@
+variable "table_name" {
+  type        = string
+  description = "DynamoDB table name"
+}
+
+variable "hash_key" {
+  type        = string
+  description = "Primary hash key for the DynamoDB table"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags for the DynamoDB table"
+  default     = {}
+}
