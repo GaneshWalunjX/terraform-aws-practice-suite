e55d4cd5bd97d69f58fd5041c85f17c0f2bcba0f
gana <ganesha.swc@gmail.com>
2025-12-06 20:48:15 +0530

    Add VPC

---
+++ Day7-mini-project/modules/vpc/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
