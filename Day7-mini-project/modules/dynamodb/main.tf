e953b780730b65b7529611ffc567e2eb0bc2d6e4
gana <ganesha.swc@gmail.com>
2025-12-15 21:11:01 +0530

    update

--- Day7-mini-project/modules/dynamodb/main.tf
+++ Day7-mini-project/modules/dynamodb/main.tf
@@ -1,11 +1,11 @@
 resource "aws_dynamodb_table" "table" {
-  name         = var.table_name
+  name             = var.table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = var.hash_key
+  hash_key       = var.hash_key
 
   attribute {
     name = var.hash_key
-    type = "S"
+    type   = "S"
   }
 
   tags = var.tags
