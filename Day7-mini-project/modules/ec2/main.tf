05102abf03a47e85519a30e48e8b459181f8edd9
gana <ganesha.swc@gmail.com>
2025-12-17 19:18:38 +0530

    update

--- Day7-mini-project/modules/ec2/main.tf
+++ Day7-mini-project/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = var.ami_id
+  ami                 = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  subnet_id       = var.subnet_id
 
   tags = {
     Name = var.ec2_name
--- Day7-mini-project/modules/ec2/variables.tf
+++ Day7-mini-project/modules/ec2/variables.tf
@@ -1,19 +1,19 @@
 variable "subnet_id" {
-  type        = string
+  type            = string
   description = "Subnet ID where EC2 will be launched"
 }
 
 variable "instance_type" {
-  type        = string
+  type            = string
   description = "Type of the EC2 instance"
 }
 
 variable "ec2_name" {
-  type        = string
+  type            = string
   description = "Name tag for the EC2 instance"
 }
 
 variable "ami_id" {
-  type        = string
+  type            = string
   description = "AMI ID for the EC2 instance"
 }
