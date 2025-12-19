041ecb87c307b094805f387ff0debda9465d5b19
gana <ganesha.swc@gmail.com>
2025-12-19 22:16:22 +0530

    update

--- Day7-mini-project/variables.tf
+++ Day7-mini-project/variables.tf
@@ -1,62 +1,62 @@
 variable "region" {
   description = "AWS region"
-  type        = string
+  type            = string
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  type        = string
+  type            = string
 }
 
 variable "vpc_name" {
   description = "Name tag for the VPC"
-  type        = string
+  type            = string
 }
 
 variable "subnet_name" {
   description = "Name tag for the subnet"
-  type        = string
+  type            = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type        = string
+  type            = string
 }
 
 variable "ec2_name" {
   description = "Name tag for EC2"
-  type        = string
+  type            = string
 }
 
 variable "ami_id" {
-  type        = string
+  type            = string
   description = "AMI ID for EC2 instance"
 }
 
 
 variable "bucket_name" {
   description = "Name of the S3 bucket"
-  type        = string
+  type            = string
 }
 
 variable "s3_tags" {
   description = "Tags for the S3 bucket"
-  type        = map(string)
-  default     = {}
+  type            = map(string)
+  default        = {}
 }
 
 variable "table_name" {
   description = "DynamoDB table name"
-  type        = string
+  type            = string
 }
 
 variable "hash_key" {
   description = "Hash key of DynamoDB table"
-  type        = string
+  type            = string
 }
 
 variable "dynamodb_tags" {
   description = "Tags for DynamoDB table"
-  type        = map(string)
-  default     = {}
+  type            = map(string)
+  default        = {}
 }
