dd4c7f448e8a447785882a16fa9676abb268e3c1
gana <ganesha.swc@gmail.com>
2025-12-21 20:41:52 +0530

    update

--- Day5-multi-environment-setup/prod/variables.tf
+++ Day5-multi-environment-setup/prod/variables.tf
@@ -1,21 +1,21 @@
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
-  type        = string
+  type            = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+  type            = string
+  default        = "t2.micro"
 }
 
 variable "instance_name" {
   description = "Name tag for EC2 instance"
-  type        = string
+  type            = string
 }
 
 variable "environment" {
   description = "Environment tag (Dev, Staging, Prod)"
-  type        = string
+  type            = string
 }
 
